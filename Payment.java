6ba3d93a5e09776b014fd37f315eb1e2fc5cf9e1
Cuong Nguyen <cpn216@lehigh.edu>
2015-09-27 15:20:33 -0400

    no message

--- Payment.java
+++ Payment.java
@@ -6,7 +6,7 @@ public class Payment {
     
     //should we make a Money class or use int as field?
     //private Money amount;
-    //private int amount;
+    private double amount;
     
     public Payment(Money cashTendered) {
         amount = cashTendered;
