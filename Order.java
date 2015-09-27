3dc2b1f21d168e367d7021f1e6d09c707b8e9175
ajp217 <ajp217@lehigh.edu>
2015-09-27 19:59:33 +0000

    Order.java

---
+++ Order.java
@@ -0,0 +1,65 @@
+import java.util.Date;
+import java.util.Calender;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order{
+
+    private Date date;
+    private Calender time;
+    private int balance;
+    private int total;
+    private int payment;
+    private int tax=.06;
+    private boolean complete;
+
+    private List <SalesLineItem> saleslineitem = new ArrayList()<SalesLineItem>;
+
+
+    public Order(){
+        date = new Date();
+        //date.toString()
+        time = Calender.getinstance();
+        //cal.getTime()
+        complete=false;
+        
+    }
+
+    public int getBalance(){
+
+        return balance;
+
+    }
+
+    public void completeOrder(){
+        complete =true;
+
+    }
+
+    public boolean isComplete(){
+        return complete;
+    }
+
+    public void addLineItem(SalesLineItem item){
+
+        saleslineitem.add(item);
+
+    }
+
+    public int getTotal(){
+
+        return total;
+
+    }
+
+    public void makePayment(){
+    
+    }
+
+    public int getTax(){
+    
+        return tax;
+    
+    }
+
+}
