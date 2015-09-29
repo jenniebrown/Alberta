6f41eef8b94560d905a282a247a31e73f8184240
Bruke Mammo <bam417@lehigh.edu>
2015-09-29 18:37:03 -0400

    initial product catalog skeleton file

---
+++ ProductCatalog.java
@@ -0,0 +1,29 @@
+/*
+ * Written by: Bruke Mammo
+ * Keeps track of all the potentially
+ * available items within the store and
+ * also acts like a backup system for
+ * tracking items.
+ */
+
+import java.util.HashMap;
+
+public class ProductCatalog{
+  
+  private HashMap<String, Item> wholeList;
+  private static ProductCatalog only;
+  
+  private ProductCatalog(){};
+  
+  public static synchronized ProductCatalog getInstance(){
+    if(only == null){
+      only = new ProductCatalog();
+    }
+    return only;
+  }
+  
+  public Item getItem(String id){   
+    Item item = wholeList.get(id);
+    return item;     
+  }
+}
