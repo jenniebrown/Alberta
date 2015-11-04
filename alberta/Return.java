5e657eaea453795d3ae38fdd74eda8e48d4ad531
Giancarlo Sanguinetti <gbs215@lehigh.edu>
2015-11-03 22:09:57 -0500

    Finished Return, Created ReturnFacade, finished return case of UI

--- alberta/Return.java
+++ alberta/Return.java
@@ -1,77 +1,46 @@
 package alberta;
 
-import java.util.Date;
 public class Return
 {
   private Rental returningRental;
-  private double feeDue;
   private double lateFee;
-  private Date dateRented;
-  private Date dateDue;
-  private Date dateReturned;
-  private String renterName;
-  private DatabaseHandler connection;
-  private Payment receivedPayment;
+  private Payment lateFeePaid;
   private boolean paidInFull;
   
   /* Constructor. Only required the Rental being returned and the Date it is being returned */
-  public Return (Rental returning, Date returned)
+  public Return (Rental returning)
   {
     this.returningRental = returning;
-    this.feeDue = returningRental.getFinalTotal();
-    this.lateFee = 0.0;
-    this.dateRented = returningRental.date; //I hope this is right
-    this.dateDue = Rental
-    this.dateReturned = returned;
-    this.renterName = returning.getRenter();
-    this.receivedPayment = null;
-    this.paidInFull = false;
-    this.connection = DatabaseHandler.connect();
+    this.lateFee = 0.0; //later should be this.surcharge()
+    this.paidInFull = this.lateFee == 0.0; //Should rely on surcharge
   }
   
   /* Getter Methods */
   public Rental getRental() { return returningRental;}
-  public double getFeeDue() { return feeDue;}
-  public Date getDateRented() { return dateRented;}
-  public Date getDateDue() { return dateDue;}
-  public Date getDateReturned() { return dateReturned;}
-  public String getRenterName() { return renterName;}
   public boolean isPaid() { return paidInFull;}
+  public double getLateFee() {return lateFee;}
   
   /* This allows for payment to be made for the return. It will only accept full payment */
-  public boolean pay (Payment payForReturn)
+  public boolean pay (Payment payLateFee)
   {
-    if (payForReturn.getAmount() == (this.feeDue + this.lateDue))
-    {
-      this.paidInFull = true;
-      this.receivedPayment = payForReturn;
-      return true;
-    }
-    else
-    {
-      return false;
-    }
+    this.lateFeePaid = payLateFee;
+    return this.paidInFull = true;
   }
   
+  
   /* This method checks to see if the item was returned late. If it was, it applies a surcharge
-   * 15% of the original rental fee per day. */
+   * 15% of the original rental fee per day. Go through the items ArrayList
+  and check their dates
   public double surcharge ()
   {
     if (!this.dateDue.equals(this.dateReturned))
     {
-      //Compare days difference, * .15
+      int daysBetween = TimeUnit.Milliseconds.toDays(this.dateReturned - 
+              returningRental.date);
     }
     else
       return 0.0;
   }
-   
-  /* Generally called after rental is made. Will update database, close connection.
-   * Note that a rental might not necessarily be paid off
-   */
-  public void updateRentalDatebase()
-  {
-    //Need to know how to update database, if one is available
-    connection.disconnect();
-  }
+  */
 }
 
